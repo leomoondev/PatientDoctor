7c43928f6494996935eb6521de166dc1e0e37ce7
Leo Moon <leomoondev@gmail.com>
2017-02-09 14:21:20 -0800

    Initial Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Hyung Jip Moon on 2017-02-09.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
